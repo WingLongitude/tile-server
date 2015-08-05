803c6b7074a0de79faff5fe454f96f5f3451d869
Christian Gendreau <christiangendreau@gmail.com>
2015-08-05 12:14:19 -0400

    fixed test

--- test/base_tests.js
+++ test/base_tests.js
@@ -3,7 +3,7 @@ var tileserver = require('../tile-server.js');
 
 describe('homepage', function(){
   it('should get 200 from GET on homepage',function(){
-    request('http://localhost:4000'), function(error, response, body) {
+    request('http://localhost:4000', function(error, response, body) {
       expect(response.statusCode).to.equal(200);
     });
   });
